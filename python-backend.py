7b8b6b0ac6b7113dce2e572b20659ab66fb37506
Aashay Parab <aashay_parab@ucsb.edu>
2024-04-28 09:52:58 -0700

    Changes to backend post request

--- python-backend.py
+++ python-backend.py
@@ -12,44 +12,46 @@ import os
 import json 
 
 app = FastAPI()
+load_dotenv()
+genai.configure(api_key=os.getenv("API_KEY"))
 
 # Function to send request to gemini
 def ask_gemini(file_path: str, name_of_file: str):
 
-    load_dotenv()
-    genai.configure(api_key=os.getenv("API_KEY"))
-
     sample_file = genai.upload_file(path=file_path, display_name=name_of_file)
 
     file = genai.get_file(name=sample_file.name)
-    print(f"Retrieved file '{file.display_name}' as: {sample_file.uri}")
+    #print(f"Retrieved file '{file.display_name}' as: {sample_file.uri}")
 
     # Set the model to Gemini 1.5 Pro.
     model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
 
     response = model.generate_content([sys_prompt, sample_file])
-    print(response.text)
+    #print(response.text)
     return response.text
 
 
 def process_response(resp: str):
-    print("Processing the gemini response: " + resp)
+    #print("Processing the gemini response: " + resp)
     recyclable = resp[resp.find("Recyclable:")+11:resp.find("Item")]
     item = resp[resp.find("Item:")+5:resp.find("Trash Type")]
     trash_type = resp[resp.find("Trash Type:")+11:resp.find("Material")]
     item_material = resp[resp.find("Material:")+9:]
 
     values = {"Recyclable": recyclable.strip(), "Item": item.strip(), "Trash Type": trash_type.strip(), "Material": item_material.strip()}
-    print(values)
+    #print(values)
     return values
 
 # The endpoint to receive images from the React App 
-@app.post("/send-image")
+@app.post("/send-image/")
 async def send_image(file: UploadFile = File(...)):
     try: 
         os.makedirs('uploads', exist_ok=True)
         file_path = f"uploads/{file.filename}"
+        #print(file_path)
+        
         name_of_file = file.filename[:file.filename.find(".")]
+        #print(name_of_file)
         with open(file_path, "wb") as f: 
             contents = await file.read()
             f.write(contents)
